da0afc792b929d911b0968ba4a5f1f11c9c250d3
maria <parody@uw.edu>
2014-04-11 18:54:49 -0700

    playing with stupid joystick. and changing specs. meh.

--- test_trainBananas.sh
+++ test_trainBananas.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp testing_config.py config.py
-ppython trainBananas.py -sTest --no-eeg --js-zero-threshold=0.03 --resolution=1024x768
+#ppython trainBananas.py -sTest --no-eeg --resolution=1024x768
+ppython trainBananas.py -sTest --no-eeg --js-zero-threshold=0.1 --resolution=1024x768
 echo "Thanks for playing."
