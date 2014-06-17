085859a4ce47b833859b58dfc31e4558530a4f8a
Maria <parody@uw.edu>
2014-06-17 12:45:16 -0700

    testing requirement for committing now working

--- test_everything.sh
+++ test_everything.sh
@@ -5,12 +5,16 @@ OUTPUT="A test has failed, please troubleshoot test suite "
 if [ "$arch" == 'x86_64' ]
 then
     for i in {0..3}; do
-        arch -i386 ppython test_trainingBananas.py $i &> /dev/null || echo $OUTPUT $i && exit 1
+        arch -i386 ppython test_trainingBananas.py $i &> /dev/null || { echo $OUTPUT $i; exit 1;}
     done
 else
+    #ppython test_trainingBananas.py 1 &> /dev/null || { echo hello; exit 1; }
+    echo do some tests
     for i in {0..3}; do
-        ppython test_trainingBananas.py $i &> /dev/null || echo $OUTPUT $i && exit 1
+        echo $i
+        ppython test_trainingBananas.py $i &> /dev/null || { echo $OUTPUT $i; exit 1; }
+        echo yup
     done
 fi
-ppython test_moBananas.py &> /dev/null || echo A test in moBananas failed && exit 1
+#ppython test_moBananas.py &> /dev/null || echo A test in moBananas failed && exit 1
 
