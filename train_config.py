ca32f2c17ef5f0e1c1e3101fa4aa77586fdc89db
codedragon <mariak@mariakathryn.net>
2014-07-01 16:04:48 -0700

    just commented out some print statements

--- train_config.py
+++ train_config.py
@@ -1,8 +1,8 @@
 # change individual config files, and have script copy to config.py
 # configuration file for trainBananas
 #
-subject = 'Test'
-#subject = 'MP'
+#subject = 'Test'
+subject = 'MP'
 
 # direction subject has to push the joystick to get the banana
 #trainingDirection = 'Right'
@@ -51,25 +51,27 @@ training = 2.3
 # training 3, move crosshair to banana, forward
 #               self.go_forward = True
 
-avatar_start_h = 1
+avatar_start_h = 1.5
 
 # random selection used for training 2.3 and above
 #random_choices = [1.5, 2, 2.3, 3, 3.4]
 random_choices = [1.5, 2.3, 3, 3.4, 4.2, 5]
 #random_choices = [1.5, 2.3, 3.4, 5, 7.6, 11]
 
-# amount need to hold crosshair on banana to get reward (2.3)
+# amount of time need to hold crosshair on banana to get reward
+# For training 2.3 and higher.
 # must be more than zero. At 1.5 distance, must be greater than
 # 0.5 to require stopping
 hold_aim = 0.6
 
 # what speed to start out at this will be multiplied by pressure on joystick
 # and time since last screen refresh
-initial_speed = 0.5
+#initial_speed = 0.5
+initial_speed = 1
 
 # Are we giving rewards? If true but no pydaq, just won't send pulse.
-reward = False
-#reward = True
+#reward = False
+reward = True
 
 # # for activating reward system
 pulseInterval = 0.2  # in seconds
