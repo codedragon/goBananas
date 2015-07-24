b2768e35fca311a46bd5aead3d78597d813476dd
Maria <mariak@mariakathryn.net>
2015-07-24 14:20:51 -0700

    added pause while playing sound

--- bananaRecall.py
+++ bananaRecall.py
@@ -247,6 +247,8 @@ class BananaRecall:
         else:
             self.reward_sound.play()
             print('beep', self.fruit.beeps)
+            time.sleep(1)
+
         # if this is first reward, log that
         if self.fruit.beeps == 0:
             VLQ.getInstance().writeLine("Yummy", [self.fruit.current_fruit])
